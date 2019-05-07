a3dc2059d0eb0720b89790bf340591b5a98cf152
Ronyonka <rononyonka@gmail.com>
2019-05-07 15:09:52 +0300

    added comments to the code

--- text.py
+++ text.py
@@ -1,14 +1,13 @@
-import textwrap
+import textwrap  # importing the textwrap module which is used in wrapping and formatting of plain text
 
-def smart_collect():
-    text = input('Please Enter a text: ')
-    dedented_text = textwrap.dedent(text).strip()
-    wrap_text = textwrap.wrap(dedented_text, width=160)
-    return wrap_text
+def text_display():
+    value = input('Please Enter a text: ')
+    dedented = textwrap.dedent(value).strip() # dedent is used to remove any common leading whitespace from every line in the input text.
+    wrapped = textwrap.wrap(dedented, width=160) # wrap function wraps the input paragraph such that each line in the paragraph is at most 160 characters long.
+    return wrapped # returns a value once the function is carried out
 
-
-
-list_strings = smart_collect()
-max_page = len(list_strings)
-for i in range(0, max_page):
-    print(f'{list_strings[i]} ({i+1}/{max_page})')
+lists = text_display() # assigns the returned value from the text_display function a variable to be stored in for simple refrence
+pages = len(lists)  # calculates the number of pages
+for i in range(0, pages):
+    print("") # empty line for aesthetic
+    print(f'{lists[i]} ({i+1}/{pages})') # displays the information/texts
